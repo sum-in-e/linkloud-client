90b852ad70beb0fe99d61eef82367509c061ef75
sumin <suminkim.me@gmail.com>
2023-06-10 23:40:48 +0900

    Fix: theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     extend: {
       colors: {
         primary: '#000DFF',
+        'primary-darker': '#000acc',
+        'primary-lighter': '#000acc',
         secondary: '#FFC107',
+        'secondary-darker': '#e6ac00',
+        'secondary-lighter': '#ffcc33',
       },
     },
   },
