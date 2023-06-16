6b339d4aba4b571881413c768758b363fde2fbfa
sumin <suminkim.me@gmail.com>
2023-06-16 19:31:23 +0900

    Fix: Loader component style

--- src/features/auth/signup/components/Loader.tsx
+++ src/features/auth/signup/components/Loader.tsx
@@ -3,7 +3,7 @@ import { AiOutlineLoading3Quarters } from 'react-icons/ai';
 function Loader() {
   return (
     <div className="flex items-center justify-center">
-      <AiOutlineLoading3Quarters className="h-4 w-4 animate-spin fill-stone-100" />
+      <AiOutlineLoading3Quarters className="h-5 w-5 animate-spin fill-stone-100" />
     </div>
   );
 }
