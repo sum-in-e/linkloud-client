d56bbd7b18dc71f7ed2a93849731c2f1a49ec74f
sumin <suminkim.me@gmail.com>
2023-06-18 16:34:34 +0900

    Add: 랜딩, 로그인, 회원가입 페이지에 로그인한 유저 접근 시 리다이렉트

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,11 +1,19 @@
 import { LINKLOUD_SLOGAN } from '@/common/modules/constants/brand';
+import { getSessionWithJWT } from '@/common/modules/utils/session';
 import SignUp from '@/features/auth/signup/containers';
+import { redirect } from 'next/navigation';
 
 export const metadata = {
   title: 'Linkloud | 회원가입',
   description: LINKLOUD_SLOGAN,
 };
 
-export default function SignUpPage() {
+export default async function SignUpPage() {
+  const isLogin = await getSessionWithJWT();
+
+  if (isLogin) {
+    redirect('/mykloud');
+  }
+
   return <SignUp />;
 }
