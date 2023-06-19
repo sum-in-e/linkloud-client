20504a2795c2fd124f85ab1f7ce9512938034d3d
sumin <suminkim.me@gmail.com>
2023-06-19 18:57:52 +0900

    Remove: client_in 사용 로직 걷어내기

--- src/features/auth/login/containers/index.tsx
+++ src/features/auth/login/containers/index.tsx
@@ -6,36 +6,23 @@ import KakaoButton from '@/features/auth/common/containers/KakaoButton';
 import EmailLogInForm from '@/features/auth/login/containers/EmailLogIn';
 import SignLayout from '@/features/auth/common/components/SignLayout';
 import { useToast } from '@chakra-ui/react';
-import { useRouter, useSearchParams } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
 import { useEffect } from 'react';
-import Cookies from 'js-cookie';
 
 const LogIn = () => {
   const toast = useToast();
-  const router = useRouter();
   const params = useSearchParams();
   const error = params.get('error');
 
   useEffect(() => {
     if (error) {
       const title = `${error}`;
-
       toast({
         title,
         status: 'error',
         duration: 3000,
         isClosable: true,
-        onCloseComplete: () => {
-          router.replace('/login');
-        },
       });
-    }
-  }, [error]);
-
-  useEffect(() => {
-    const isLoggedIn = Cookies.get('client_in') === 'true';
-    if (isLoggedIn) {
-      router.push('/mykloud');
     }
   }, []);
 
