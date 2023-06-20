2e3b3cf861705922cfb48d14c203f887adf3b36b
sumin <suminkim.me@gmail.com>
2023-06-20 13:58:39 +0900

    Remove: 서버 컴포넌트 세션 요청 걷어내기

--- src/features/auth/common/modules/apis/session.ts
+++ src/features/auth/common/modules/apis/session.ts
@@ -1,6 +1,5 @@
-import { instance, instanceForServer } from '@/common/modules/apis/instance';
+import { instance } from '@/common/modules/apis/instance';
 import { SignUpMethodType } from '@/features/auth/signup/modules/types/signupType';
-import { cookies } from 'next/headers';
 
 export type GetMeResponse = SuccessResponseType<{
   id: number;
@@ -11,21 +10,5 @@ export type GetMeResponse = SuccessResponseType<{
 
 export const getSession = async (): Promise<GetMeResponse> => {
   const { data } = await instance.get(`/user/me`);
-  return data;
-};
-
-/**
- * 서버 컴포넌트에서 세션 확인 API 요청할 경우 사용 - 쿠키가 자동으로 요청 헤더에 실리지 않기 때문에 직접 넣어줍니다.
- */
-export const getSessionInSever = async (): Promise<GetMeResponse> => {
-  const cookieStore = cookies();
-  const sq = cookieStore.get('sq')?.value;
-  const bp = cookieStore.get('bp')?.value;
-
-  const { data } = await instanceForServer.get(`/user/me`, {
-    headers: {
-      Cookie: `sq=${sq}; bp=${bp}`,
-    },
-  });
   return data;
 };
