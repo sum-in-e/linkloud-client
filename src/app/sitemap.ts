2ac2e5bf44800e7fc1f715ec922c9e25038b7633
sumin <suminkim.me@gmail.com>
2023-06-21 20:15:19 +0900

    Fix: sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,23 +3,23 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https:/linkloud.co.kr',
+      url: 'https://linkloud.co.kr',
       lastModified: new Date(),
     },
     {
-      url: 'https:/linkloud.co.kr/login',
+      url: 'https://linkloud.co.kr/login',
       lastModified: new Date(),
     },
     {
-      url: 'https:/linkloud.co.kr/signup',
+      url: 'https://linkloud.co.kr/signup',
       lastModified: new Date(),
     },
     {
-      url: 'https:/linkloud.co.kr/kloud',
+      url: 'https://linkloud.co.kr/kloud',
       lastModified: new Date(),
     },
     {
-      url: 'https:/linkloud.co.kr/setting',
+      url: 'https://linkloud.co.kr/setting',
       lastModified: new Date(),
     },
   ];
