95d5c6d9f4547de8bb397e9ce407c62fa018242e
sumin <suminkim.me@gmail.com>
2023-06-26 11:38:15 +0900

    Add: startup images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,11 +45,42 @@ export const metadata = {
   // For PWA
   applicationName: 'Linkloud',
   manifest: 'https://linkloud.co.kr/manifest.json',
+  themeColor: 'white',
   appleWebApp: {
     title: 'Linkloud',
     statusBarStyle: 'black-translucent',
+    startupImage: [
+      {
+        url: '/startup/apple-splash-750x1334.png',
+        media: '(device-width: 375px) and (-webkit-device-pixel-ratio: 2)',
+      },
+      {
+        url: '/startup/apple-splash-828x1792.png',
+        media:
+          '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)',
+      },
+      {
+        url: '/startup/apple-splash-1125x2436.png',
+        media:
+          '(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)',
+      },
+      {
+        url: '/startup/apple-splash-1170x2532.png',
+        media:
+          '(device-width:390px) and (device-height:844px) and (-webkit-device-pixel-ratio:3)',
+      },
+      {
+        url: '/startup/apple-splash-1242x2688.png',
+        media:
+          '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio:3)',
+      },
+      {
+        url: '/startup/apple-splash-1284x2778.png',
+        media:
+          '(device-width:428px) and (device-height:926px) and (-webkit-device-pixel-ratio:3)',
+      },
+    ],
   },
-  themeColor: 'black',
 };
 
 export default function RootLayout({
