c20624ef22c4b7eba48feec2fea75b752003390d
sumin <suminkim.me@gmail.com>
2023-06-27 19:00:40 +0900

    Fix: SignOut

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
       colors: {
         primary: '#000DFF',
         'primary-darker': '#000acc',
-        'primary-lighter': '#000acc',
+        'primary-lighter': '#333dff',
         secondary: '#FFC107',
         'secondary-darker': '#e6ac00',
         'secondary-lighter': '#ffcc33',
