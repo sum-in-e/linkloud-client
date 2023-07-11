c7e6d37fc2cdf3f4e0f74bef938907e6eb121237
sumin <suminkim.me@gmail.com>
2023-07-11 10:36:29 +0900

    remove: prefetch false

--- src/features/kloud/components/MenuItem.tsx
+++ src/features/kloud/components/MenuItem.tsx
@@ -15,7 +15,6 @@ const MenuItem = ({
     <Link
       href={href}
       className="flex w-full items-center justify-between rounded-2xl bg-slate-200 px-4 py-3 text-sm font-bold"
-      prefetch={false}
     >
       <p className="">{title}</p>
       <p className="">{count}</p>
