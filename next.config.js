274d7a2f946320bf642b4aa6118c3ab3433ac81f
sumin <suminkim.me@gmail.com>
2023-07-13 00:42:50 +0900

    Add: exclude in next.config.js

--- next.config.js
+++ next.config.js
@@ -10,6 +10,7 @@ const withPWA = require('next-pwa')({
   customWorkerDir: '/public/worker', // 커스텀 서비스 워커 폴더를 지정 (worker 폴더에 있는 파일들이 next-pwa가 생성한 sw.js 파일에 추가된다.)
   runtimeCaching, //PWA가 오프라인 작동을 지원하기 위해 캐싱해주는 역할
   // disable: process.env.NEXT_PUBLIC_MODE === 'development', // disable is help to disable PWA in deployment mode
+  exclude: ['_next/app-build-manifest.json'], // exclude this file from precache
 });
 
 const nextConfig = {
