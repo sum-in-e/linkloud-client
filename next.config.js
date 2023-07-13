4f232a711b55bf57cb9ba05dfbefd67e2cb26510
sumin <suminkim.me@gmail.com>
2023-07-13 12:51:05 +0900

    test: service worker

--- next.config.js
+++ next.config.js
@@ -3,17 +3,17 @@
 const { withSentryConfig } = require('@sentry/nextjs');
 const runtimeCaching = require('next-pwa/cache');
 
-// const withPWA = require('next-pwa')({
-//   dest: 'public', // 서비스 워커 파일과 관련된 파일들을 public 폴더에 저장
-//   register: true, // 직접 등록할거면 여기서 false 처리해야 중복으로 등록되지 않는다.
-//   skipWaiting: true, // 새로운 서비스 워커가 설치되자마자 이전 버전의 서비스 워커를 대체하도록 설정. => 업데이트시 빠르게 적용됨
-//   customWorkerDir: '/public/worker', // 커스텀 서비스 워커 폴더를 지정 (worker 폴더에 있는 파일들이 next-pwa가 생성한 sw.js 파일에 추가된다.)
-//   runtimeCaching, //PWA가 오프라인 작동을 지원하기 위해 캐싱해주는 역할
-//   // disable: process.env.NEXT_PUBLIC_MODE === 'development', // disable is help to disable PWA in deployment mode
-//   exclude: ['_next/app-build-manifest.json'], // exclude this file from precache
-// });
+const withPWA = require('next-pwa')({
+  dest: 'public', // 서비스 워커 파일과 관련된 파일들을 public 폴더에 저장
+  // register: true, // next-pwa가 서비스워커를 자동으로 등록
+  // skipWaiting: true, // 새로운 서비스 워커가 설치되자마자 이전 버전의 서비스 워커를 대체하도록 설정. => 업데이트시 빠르게 적용됨
+  customWorkerDir: 'src/worker', // 커스텀 서비스 워커 폴더를 지정 (worker 폴더에 있는 파일들이 next-pwa가 생성한 sw.js 파일에 추가된다.)
+  // runtimeCaching, //PWA가 오프라인 작동을 지원하기 위해 캐싱해주는 역할
+  // disable: process.env.NEXT_PUBLIC_MODE === 'development', // disable is help to disable PWA in deployment mode
+  // exclude: ['_next/app-build-manifest.json'], // exclude this file from precache
+});
 
-const nextConfig = {
+const nextConfig = withPWA({
   async redirects() {
     return [
       {
@@ -23,7 +23,7 @@ const nextConfig = {
       },
     ];
   },
-};
+});
 
 const sentryConfig = {
   hideSourceMaps: true,
@@ -36,4 +36,3 @@ const sentryConfig = {
 };
 
 module.exports = withSentryConfig(nextConfig, sentryConfig);
-// module.exports = withSentryConfig(withPWA(nextConfig), sentryConfig);
---
+++ src/worker/index.ts
@@ -0,0 +1,91 @@
+/// <reference lib="webworker" />
+
+// eslint-disable-next-line no-undef
+export declare const self: ServiceWorkerGlobalScope;
+
+self.addEventListener('install', (e) => {
+  console.log('👀 - install', e);
+  e.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', (e) => {
+  console.log('👀 - activate', e);
+});
+
+self.addEventListener('push', (e) => {
+  const message = e.data?.json();
+  console.log('👀 - message', message);
+
+  e.waitUntil(
+    self.registration.showNotification(message.sender.nickname, {
+      body: message.content,
+      icon: message.sender.imageUrl,
+      data: message.url,
+    })
+  );
+});
+
+self.addEventListener('notificationclick', (e) => {
+  self.clients.openWindow(e.notification.data);
+});
+
+// listen to message event from window
+self.addEventListener('message', (e) => {
+  // HOW TO TEST THIS?
+  // Run this in your browser console:
+  //     window.navigator.serviceWorker.controller.postMessage({command: 'log', message: 'hello world'})
+  // OR use next-pwa injected workbox object
+  //     window.workbox.messageSW({command: 'log', message: 'hello world'})
+  console.log(e.data);
+});
+
+// 서비스 워커 설치할 때
+// self.addEventListener('install', (event) => {
+//   // 제어중인 서비스 워커가 존재해도 대기 상태를 건너뛴다.
+//   console.log('install');
+//   self.skipWaiting();
+// });
+
+// 서비스 워커 설치 중일 때
+// self.addEventListener('activate', (event) => {
+// });
+
+/**
+ * @description 서비스워커에서 발생하는 푸시 이벤트를 수신한다.(서버에서 푸시 이벤트 보내도록 구현)
+ * self는 서비스워커를 참조한다.
+ */
+// self.addEventListener('push', (event) => {
+//   console.log('[Service Worker] Push Received.', event.data.text());
+//   const data = event.data.json();
+//   const title = data.title || '흥미로운 링크를 담아두셨네요?👀';
+//   const options = {
+//     body: data.description || '저장한 글을 읽고 더 성장한 나를 만나보세요!', // 푸시 알림 본문
+//     icon: 'https://res.cloudinary.com/dqcgvbbv7/image/upload/v1687269892/linkloud/emtygeehcgigfn9wlhw3.jpg', // 푸시 알림에 표시될 아이콘
+//     badge:
+//       'https://res.cloudinary.com/dqcgvbbv7/image/upload/v1687269892/linkloud/emtygeehcgigfn9wlhw3.jpg', // 푸시 알림 배지 이미지 / android에서만 보인다.
+//     actions: [
+//       {
+//         action: 'show-unread-action',
+//         title: '지금 보러가기',
+//       },
+//     ],
+//     requireInteraction: true, // chrome과 같이 충분히 큰 창에서 사용자가 직접 닫을 때까지 알림 사라지지 않음
+//   };
+
+//   event.waitUntil(self.registration.showNotification(title, options));
+// });
+
+/**
+ * @description push notification 클릭 시 동작 정의
+ */
+// self.addEventListener('notificationclick', (event) => {
+//   // 알림창 닫기
+//   event.notification.close();
+
+//   if (event.action === 'show-unread-action') {
+//     // 지금 확인하기 버튼 클릭 시 미열람 페이지로 이동
+//     event.waitUntil(clients.openWindow('https://linkloud.co.kr/kloud/unread'));
+//   }
+
+//   // 알림 클릭 시 발생할 기본 액션도 정의 가능
+// });
