2979d37831f0ce2b2e830262971774eaed9b742f
sumin <suminkim.me@gmail.com>
2023-07-13 18:12:23 +0900

    Test: 알림 발송 배포 테스트

--- next.config.js
+++ next.config.js
@@ -5,11 +5,11 @@ const runtimeCaching = require('next-pwa/cache');
 
 const withPWA = require('next-pwa')({
   dest: 'public', // 서비스 워커 파일과 관련된 파일들을 public 폴더에 저장
-  // register: true, // next-pwa가 서비스워커를 자동으로 등록
-  // skipWaiting: true, // 새로운 서비스 워커가 설치되자마자 이전 버전의 서비스 워커를 대체하도록 설정. => 업데이트시 빠르게 적용됨
+  register: true, // next-pwa가 서비스워커를 자동으로 등록
+  skipWaiting: true, // 새로운 서비스 워커가 설치되자마자 이전 버전의 서비스 워커를 대체하도록 설정. => 업데이트시 빠르게 적용됨
   customWorkerDir: 'src/worker', // 커스텀 서비스 워커 폴더를 지정 (worker 폴더에 있는 파일들이 next-pwa가 생성한 sw.js 파일에 추가된다.)
-  // runtimeCaching, //PWA가 오프라인 작동을 지원하기 위해 캐싱해주는 역할
-  // disable: process.env.NEXT_PUBLIC_MODE === 'development', // disable is help to disable PWA in deployment mode
+  runtimeCaching, //PWA가 오프라인 작동을 지원하기 위해 캐싱해주는 역할
+  disable: process.env.NEXT_PUBLIC_MODE === 'development', // disable is help to disable PWA in deployment mode
   buildExcludes: ['app-build-manifest.json'], // bad-precaching-response 이슈 해결 -> 중복됨 상태도 사라짐
 });
 
