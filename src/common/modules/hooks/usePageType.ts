3355ead21256a2db7c710e3951ddbe6d842a1bac
sumin <suminkim.me@gmail.com>
2023-07-18 23:32:55 +0900

    Fix: 페이지별 헤더 다르게 수정

---
+++ src/common/modules/hooks/usePageType.ts
@@ -0,0 +1,26 @@
+import { usePathname } from 'next/navigation';
+import { useMemo } from 'react';
+
+type PageType = 'public' | 'private';
+
+/**
+ * pathname에 따라 해당 페이지가 public 인지 private 페이지인지 여부를 반환합니다.
+ * @returns {PageType} 'public' | 'private'
+ * @example
+ * const pageType = usePageType(); // 'public' 또는 'private'
+ */
+export const usePageType = (): PageType => {
+  const pathname = usePathname();
+
+  const privatePaths = useMemo(() => ['/kloud', '/setting'], []);
+
+  const pageType = useMemo<PageType>(() => {
+    if (privatePaths.some((path) => pathname.includes(path))) {
+      return 'private';
+    }
+
+    return 'public';
+  }, [pathname, privatePaths]);
+
+  return pageType;
+};
