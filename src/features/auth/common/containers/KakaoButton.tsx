aaafc137e1cb3a4dbc5607354d57683a90868b2e
sumin <suminkim.me@gmail.com>
2023-07-21 11:30:25 +0900

    Fix: 준비중입니다 걷어내기

--- src/features/auth/common/containers/KakaoButton.tsx
+++ src/features/auth/common/containers/KakaoButton.tsx
@@ -1,10 +1,10 @@
 'use client';
 
-import { AuthMethodType } from '@/features/auth/common/modules/types/authType';
 import Link from 'next/link';
+import { useSearchParams } from 'next/navigation';
 import { RiKakaoTalkFill } from 'react-icons/ri';
 import * as querystring from 'querystring';
-import { useSearchParams } from 'next/navigation';
+import { AuthMethodType } from '@/features/auth/common/modules/types/authType';
 
 const KakaoButton = ({ type }: { type: AuthMethodType }) => {
   const params = useSearchParams();
@@ -15,32 +15,19 @@ const KakaoButton = ({ type }: { type: AuthMethodType }) => {
     return_to: returnUrl || '/kloud',
   });
 
-  // TODO: 걷어내기
-  // const text =
-  //   type === 'login' ? '카카오로 계속하기' : '카카오로 바로 시작하기';
-  const text = '서비스 준비중입니다.';
+  const text =
+    type === 'login' ? '카카오로 계속하기' : '카카오로 바로 시작하기';
 
   return (
     <section className="w-full">
-      <button
-        className="common-button gap-1 bg-[#FAE100] py-3  hover:bg-[#ffe81a]"
-        disabled={true}
-      >
-        <RiKakaoTalkFill />
-        <p className="text-sm font-bold text-black ">{text}</p>
-      </button>
-      {/* TODO: 위 버튼 지우고 아래꺼 활성화 */}
-      {/* <Link
+      <Link
         href={`${process.env.NEXT_PUBLIC_BASE_URL}/user/auth/kakao?${queryString}`}
       >
-        <button
-          className="common-button gap-1 bg-[#FAE100] py-3  hover:bg-[#ffe81a]"
-          disabled={true}
-        >
+        <button className="common-button gap-1 bg-[#FAE100] py-3  hover:bg-[#ffe81a]">
           <RiKakaoTalkFill />
           <p className="text-sm font-bold text-black ">{text}</p>
         </button>
-      </Link> */}
+      </Link>
     </section>
   );
 };
