6d2e042d9c68adede180f47e0c4db7660b4def6f
sumin <suminkim.me@gmail.com>
2023-07-21 11:27:21 +0900

    Fix: 마이페이지 마무리

--- src/features/setting/containers/index.tsx
+++ src/features/setting/containers/index.tsx
@@ -1,13 +1,13 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
+import { Skeleton } from '@chakra-ui/react';
+import { FaArrowLeft } from 'react-icons/fa';
 import { useGetSessionQuery } from '@/features/auth/common/modules/apiHooks/useGetSessionQuery';
-import UserInfoSkeleton from '@/features/setting/components/UserInfoSkeleton';
 import LogOutButton from '@/features/setting/containers/LogOut';
 import SignOutButton from '@/features/setting/containers/SignOut';
 import UserInfo from '@/features/setting/containers/UserInfo';
-import NotificationHandler from '@/features/setting/containers/NotificationHandler';
-import { FaArrowLeft } from 'react-icons/fa';
+import NotificationArea from '@/features/setting/containers/Notification';
 
 const MySetting = () => {
   const { isLoading, data } = useGetSessionQuery();
@@ -21,20 +21,23 @@ const MySetting = () => {
   return (
     <>
       {isLoading ? (
-        <UserInfoSkeleton />
+        <Skeleton className="h-[70vh] w-full max-w-[340px]" />
       ) : (
-        <div className="flex w-full max-w-[340px] flex-col gap-7 overflow-hidden">
+        <div className="mb-10 flex w-full max-w-[340px] flex-col gap-7 overflow-hidden">
           <button
             type="button"
             onClick={handleClick}
-            className="h-fit w-fit rounded-2xl border-[1px] border-stone-200 px-4 py-2 hover:bg-slate-200"
+            className="group h-fit w-fit rounded-2xl border-[1px] border-stone-200 px-4 py-2"
           >
-            <FaArrowLeft size={15} className="fill-gray-600" />
+            <FaArrowLeft
+              size={15}
+              className="fill-gray-600 group-hover:fill-primary"
+            />
           </button>
           <UserInfo />
-          <hr className="bordser-gray-300 w-full" />
-          <NotificationHandler />
-          <hr className="w-full border-gray-300" />
+          <Devider />
+          <NotificationArea />
+          <Devider />
           <section className="flex w-full justify-center gap-3">
             <LogOutButton />
             <SignOutButton />
@@ -46,3 +49,7 @@ const MySetting = () => {
 };
 
 export default MySetting;
+
+const Devider = () => {
+  return <hr className="w-full border-gray-300" />;
+};
