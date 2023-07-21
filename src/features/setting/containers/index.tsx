b50579e42c1a4cea32ed5bba0dbb4f030e9370e9
sumin <suminkim.me@gmail.com>
2023-07-21 11:28:32 +0900

    Fix: 뒤로가기 버튼 호버 스타일 제거

--- src/features/setting/containers/index.tsx
+++ src/features/setting/containers/index.tsx
@@ -27,12 +27,9 @@ const MySetting = () => {
           <button
             type="button"
             onClick={handleClick}
-            className="group h-fit w-fit rounded-2xl border-[1px] border-stone-200 px-4 py-2"
+            className="h-fit w-fit rounded-2xl border-[1px] border-stone-200 px-4 py-2"
           >
-            <FaArrowLeft
-              size={15}
-              className="fill-gray-600 group-hover:fill-primary"
-            />
+            <FaArrowLeft size={15} className="fill-gray-600" />
           </button>
           <UserInfo />
           <Devider />
