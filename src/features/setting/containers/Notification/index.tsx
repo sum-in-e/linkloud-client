d07cbc074476ec87a2a366c2a757dc663b4df99a
sumin <suminkim.me@gmail.com>
2023-07-22 22:11:18 +0900

    Fix: 알림 설정 핸들러 리팩토링

--- src/features/setting/containers/Notification/index.tsx
+++ src/features/setting/containers/Notification/index.tsx
@@ -1,10 +1,10 @@
-import NotificationHandler from '@/features/setting/containers/Notification/NotificationHandler';
+import NotReadNotificationHandler from '@/features/setting/containers/Notification/NotReadNotificationHandler';
 
 const NotificationArea = () => {
   return (
     <section>
-      <p className="mb-5 text-xl font-bold">알림</p>
-      <NotificationHandler />
+      <p className="mb-5 font-bold">알림 설정</p>
+      <NotReadNotificationHandler />
     </section>
   );
 };
