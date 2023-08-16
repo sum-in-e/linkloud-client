c01879d561ee81e7883f452d99318dc8360350cb
sumin <suminkim.me@gmail.com>
2023-08-16 20:04:42 +0900

    Fix: 랜딩페이지 마무리

--- src/features/randing/TopSection.tsx
+++ src/features/randing/TopSection.tsx
@@ -1,14 +1,14 @@
 'use client';
 
 import Image from 'next/image';
-import preview_pc from '/public/images/preview_pc.png';
+import preview_pc from '/public/images/preview.png';
 import preview_mobile from '/public/images/preview_mobile.png';
 import Link from 'next/link';
 
 const TopSection = () => {
   return (
-    <section className="flex w-full flex-col gap-10 px-4 pb-10 pt-32 md:flex-row md:gap-0 md:px-0 md:pb-16 md:pt-7">
-      <div className="flex w-full flex-col justify-center gap-4 md:w-2/5 md:px-4">
+    <section className="flex w-full flex-col gap-10 px-4 pb-10 pt-32 md:flex-row md:gap-0 md:px-0 md:py-7">
+      <div className="flex w-full flex-col justify-center gap-4 md:w-[40%] md:px-4">
         <h2 className="text-3xl font-bold md:text-4xl">
           나중에 볼 링크를 한곳에
         </h2>
@@ -22,8 +22,8 @@ const TopSection = () => {
           시작하기
         </Link>
       </div>
-      <div className="relative w-full md:w-3/5 md:px-4 md:py-36">
-        <div className="overflow-hidden rounded-md border border-zinc-200 shadow-sm md:-translate-y-20">
+      <div className="relative w-full md:w-[60%] md:px-4 md:py-36">
+        <div className="overflow-hidden rounded-lg border border-zinc-200 shadow-sm md:-translate-y-20">
           <Image
             src={preview_pc}
             alt="linkloud preview"
@@ -31,7 +31,7 @@ const TopSection = () => {
             priority
           />
         </div>
-        <div className="absolute bottom-6 right-6 hidden w-2/5 overflow-hidden rounded-md border border-zinc-200 shadow-sm md:block">
+        <div className="absolute bottom-6 right-6 hidden w-[30%] overflow-hidden rounded-lg border border-zinc-200 shadow-sm md:block">
           <Image
             src={preview_mobile}
             alt="linkloud mobile preview"
