bb9054509d2b48d2b3cf4065f87be20fed5f03ae
sumin <suminkim.me@gmail.com>
2023-08-17 23:16:34 +0900

    Fix: 구글애드센스 랜딩페이지에만 보이게 수정

--- src/common/containers/GoogleAdsense.tsx
+++ src/common/containers/GoogleAdsense.tsx
@@ -3,16 +3,16 @@
 import Script from 'next/script';
 
 const GoogleAdsense = () => {
-  return (
-    <>
-      <Script
-        async
-        strategy="afterInteractive"
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2646613755586698"
-        crossOrigin="anonymous"
-      ></Script>
-    </>
-  );
+  const isRandingPage = window.location.pathname === '/';
+
+  return isRandingPage ? (
+    <Script
+      async
+      strategy="afterInteractive"
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2646613755586698"
+      crossOrigin="anonymous"
+    ></Script>
+  ) : null;
 };
 
 export default GoogleAdsense;
