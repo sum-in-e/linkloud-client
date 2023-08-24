cdba1882c4cd4cb031172fb44fb96775f2fddcea
sumin <suminkim.me@gmail.com>
2023-08-24 21:27:35 +0900

    Fix: metadata for Twitter

--- src/common/modules/constants/brand.ts
+++ src/common/modules/constants/brand.ts
@@ -1,5 +1,8 @@
 export const LINKLOUD_DESCRIPTION =
   '나중에 볼 링크들, 이제는 링클라우드에 저장하고 확인하세요.';
 
+export const LINKLOUD_OG_IMAGE_URL =
+  'https://res.cloudinary.com/dqcgvbbv7/image/upload/f_auto,q_auto/v1/linkloud/emtygeehcgigfn9wlhw3';
+
 export const LINKLOUD_THUMBNAIL_IMAGE_URL =
   'https://res.cloudinary.com/dqcgvbbv7/image/upload/f_auto,q_auto/v1/linkloud/linkloud_thumbnail_cp3joj';
