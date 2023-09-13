6b8720edf269247b3836133f5cb886e0e88b3fa4
sumin <suminkim.me@gmail.com>
2023-09-13 11:30:00 +0900

    Fix: Help&Info에 url 연결

--- src/common/containers/HelpAndInfoArea/index.tsx
+++ src/common/containers/HelpAndInfoArea/index.tsx
@@ -1,22 +1,19 @@
 'use client';
 
+import Link from 'next/link';
 import { BsInfoCircle } from 'react-icons/bs';
 
 const HelpAndInfoArea = () => {
-  const handleClick = () => {
-    alert('준비중입니다.');
-  };
-
   return (
     <div className="md:mb-3 md:px-10">
-      <button
-        type="button"
-        onClick={handleClick}
+      <Link
+        target="_blank"
+        href="https://www.craft.me/s/AGjkOZUm2mFTDE"
         className="color-duration flex w-full items-center gap-3 rounded-md border px-2 py-3  text-sm font-semibold text-zinc-700 md:w-fit md:rounded-full md:border-0 md:px-3 md:py-2 md:hover:bg-zinc-200"
       >
         <BsInfoCircle size={15} className="fill-zinc-700" />
         Help & Info
-      </button>
+      </Link>
     </div>
   );
 };
