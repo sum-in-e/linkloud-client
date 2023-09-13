925d2f12aa061d1ecbeb925bd75398ccd85de9b2
sumin <suminkim.me@gmail.com>
2023-09-13 12:49:59 +0900

    Fix: 애드센스 스크립트 수동 제거

--- src/common/containers/GoogleAdsense.tsx
+++ src/common/containers/GoogleAdsense.tsx
@@ -1,20 +1,37 @@
 'use client';
 
 import { usePathname } from 'next/navigation';
-import Script from 'next/script';
+import { useEffect } from 'react';
 
 const GoogleAdsense = () => {
   const pathname = usePathname();
   const isShowAd =
     pathname === '/' || pathname === '/login' || pathname === '/signup';
 
-  return isShowAd ? (
-    <Script
-      async
-      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2646613755586698"
-      crossOrigin="anonymous"
-    ></Script>
-  ) : null;
+  useEffect(() => {
+    const scriptId = 'google-adsense-script';
+
+    // isShowAd가 true일 때 스크립트 추가
+    if (isShowAd) {
+      if (!document.getElementById(scriptId)) {
+        const script = document.createElement('script');
+        script.src =
+          'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2646613755586698';
+        script.async = true;
+        script.id = scriptId;
+        document.body.appendChild(script);
+      }
+    }
+    // isShowAd가 false일 때 스크립트 제거
+    else {
+      const existingScript = document.getElementById(scriptId);
+      if (existingScript) {
+        existingScript.remove();
+      }
+    }
+  }, [isShowAd]);
+
+  return null;
 };
 
 export default GoogleAdsense;
