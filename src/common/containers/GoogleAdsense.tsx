aa25a5e6062601a2136d06aa3c097f20c4e6c0e0
sumin <suminkim.me@gmail.com>
2023-09-13 16:39:49 +0900

    Fix: 구글 애드센스 스크립트 수정

--- src/common/containers/GoogleAdsense.tsx
+++ src/common/containers/GoogleAdsense.tsx
@@ -1,37 +1,17 @@
 'use client';
 
-import { usePathname } from 'next/navigation';
-import { useEffect } from 'react';
+import Script from 'next/script';
 
 const GoogleAdsense = () => {
-  const pathname = usePathname();
-  const isShowAd =
-    pathname === '/' || pathname === '/login' || pathname === '/signup';
-
-  useEffect(() => {
-    const scriptId = 'google-adsense-script';
-
-    // isShowAd가 true일 때 스크립트 추가
-    if (isShowAd) {
-      if (!document.getElementById(scriptId)) {
-        const script = document.createElement('script');
-        script.src =
-          'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2646613755586698';
-        script.async = true;
-        script.id = scriptId;
-        document.body.appendChild(script);
-      }
-    }
-    // isShowAd가 false일 때 스크립트 제거
-    else {
-      const existingScript = document.getElementById(scriptId);
-      if (existingScript) {
-        existingScript.remove();
-      }
-    }
-  }, [isShowAd]);
-
-  return null;
+  return (
+    <Script
+      async
+      strategy="lazyOnload"
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2646613755586698"
+      crossOrigin="anonymous"
+    ></Script>
+  );
 };
 
 export default GoogleAdsense;
+s;
