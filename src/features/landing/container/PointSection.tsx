4bd56e3c71c0e3e1955c6d0b178aec85bbdfb24c
sumin <suminkim.me@gmail.com>
2023-12-16 14:20:25 +0900

    fix: 랜딩페이지 컨텐츠 내 컬렉션 -> 나중에 볼 것으로 변경

--- src/features/landing/container/PointSection.tsx
+++ src/features/landing/container/PointSection.tsx
@@ -4,8 +4,7 @@ import grid_item_2 from '/public/images/landing/grid-item-2.webp';
 import grid_item_3 from '/public/images/landing/grid-item-3.webp';
 import grid_item_4 from '/public/images/landing/grid-item-4.webp';
 import grid_item_5 from '/public/images/landing/grid-item-5.webp';
-import grid_item_6 from '/public/images/landing/grid-item-6.webp';
-import grid_item_7 from '/public/images/landing/grid-item-7.webp';
+import gridFollowing from '/public/images/landing/grid-following.webp';
 
 const PointSection = () => {
   return (
@@ -13,12 +12,12 @@ const PointSection = () => {
       <div className="w-full max-w-screen-xl">
         <div className="flex w-full flex-col items-start gap-5 px-4 py-14 md:px-10 md:py-20 lg:flex-row">
           <div className="w-full lg:w-[30%] ">
-            <h2 className="md:reading-8 mt-3 whitespace-normal text-2xl font-semibold leading-9 text-zinc-100 md:whitespace-pre-wrap md:text-4xl">
+            <p className="md:reading-8 mt-3 whitespace-normal text-2xl font-semibold leading-9 text-zinc-100 md:whitespace-pre-wrap md:text-4xl">
               {`나만의 링크 관리 매니저를 고용하세요`}
-            </h2>
-            <h4 className="my-5 whitespace-pre-wrap text-lg font-semibold text-zinc-300 md:text-xl">
+            </p>
+            <p className="my-5 whitespace-pre-wrap text-lg font-semibold text-zinc-300 md:text-xl">
               {`링크를 관리하기 위해 들여야 했던 시간과 노력을 최소화하고 본연의 일에 더 집중할 수 있습니다.`}
-            </h4>
+            </p>
           </div>
           <div className="w-full lg:w-[60%]">
             <video className="w-full" controls muted loop autoPlay playsInline>
@@ -32,7 +31,7 @@ const PointSection = () => {
         <ul className="grid w-full grid-cols-[repeat(auto-fill,minmax(calc(50%-10px),1fr))] grid-rows-[1fr] gap-10 px-4 pb-14 md:grid-cols-[repeat(auto-fill,minmax(350px,1fr))] md:px-10 md:pb-20">
           {items.map((item, index) => (
             <li key={index}>
-              <div className="h-40 w-full overflow-hidden rounded-xl bg-zinc-50 p-2">
+              <div className="flex h-52 w-full items-center justify-center overflow-hidden rounded-xl bg-zinc-50 p-2">
                 <Image src={item.src} alt="Linkloud feature" loading="eager" />
               </div>
               <h5 className="mb-3 mt-2 text-lg font-semibold text-zinc-100">
@@ -61,33 +60,28 @@ const items = [
     src: grid_item_2,
   },
   {
-    title: '확인하지 않은 링크를 한눈에 알아보기',
+    title: '특별한 링크는 메모 남기기',
     description:
-      '확인하지 않은 링크는 노란 점으로 표시되어 미확인 링크와 쉽게 구분할 수 있습니다. 클라우드에 미확인 링크가 저장되어 있다면 클라우드에도 노란 표시가 떠요.',
+      '링크에 대한 특별한 기록을 남기고 싶다면 메모 기능을 이용해 보세요. 메모가 작성된 링크는 리스트에서 한눈에 알아볼 수 있습니다.',
     src: grid_item_3,
+  },
+  {
+    title: '나중에 볼 링크는 별도로 표시하기',
+    description:
+      '[나중에 볼 것] 메뉴에서 링크를 모아보고 관리할 수 있어요. 링크의 우측 하단에 있는 북마크 아이콘을 클릭해 등록/해제할 수 있습니다.',
+    src: gridFollowing,
+  },
+  {
+    title: '나중에 볼 링크를 한눈에 알아보기',
+    description:
+      '[나중에 볼 것] 리스트에 추가한 링크는 리스트에서 노란 점으로 표시됩니다. 놓치지 말고 확인하세요!',
+    src: grid_item_5,
   },
   {
     title: '필요했던 링크 잊지 않기',
     description:
-      '미확인 링크로 구성된 ‘지금 읽을만한 것’ 피드를 제공합니다. 일일이 들여다보는 수고를 들이지 않아도 필요한 링크를 놓치지 않을 수 있도록 도와드려요.',
+      '나중에 볼 링크로 구성된 ‘지금 읽을만한 것’ 피드를 제공합니다. 일일이 들여다보는 수고를 들이지 않아도 필요한 링크를 놓치지 않을 수 있어요.',
     src: grid_item_4,
-  },
-  {
-    title: '링크를 관리하는 수고로움에서 벗어나기',
-    description: `필요 없어진 링크를 찾고 정리하는 건 번거로운 일입니다. 링클라우드가 이런 수고를 덜어드릴게요. '링크 관리 매니저'가 데이터를 기반으로 정리가 필요한 링크를 보여드려요.`,
-    src: grid_item_5,
-  },
-  {
-    title: '특별한 링크는 메모 남기기',
-    description:
-      '링크에 대한 특별한 기록을 남기고 싶다면 메모 기능을 이용해 보세요. 메모가 작성된 링크는 리스트에서 한눈에 알아볼 수 있습니다.',
-    src: grid_item_6,
-  },
-  {
-    title: '소장용 링크는 내 컬렉션으로 분류하기',
-    description:
-      '내 컬렉션에 등록한 링크는 소장용 링크로 분류됩니다. 확인 여부를 체크할 필요가 없는 링크라면 내 컬렉션에 등록해 보세요. 분류를 통해 링크를 더욱 효율적으로 관리할 수 있습니다.',
-    src: grid_item_7,
   },
 ];
 
--- src/features/landing/container/TopSection.tsx
+++ src/features/landing/container/TopSection.tsx
@@ -6,12 +6,12 @@ const TopSection = () => {
   return (
     <section className="flex w-full justify-center">
       <div className="flex max-w-screen-xl flex-col items-center px-4 py-14 md:px-10 md:py-20">
-        <h2 className="md:reading-8 whitespace-pre-wrap text-center text-2xl font-semibold leading-9 text-zinc-800 md:whitespace-normal md:text-4xl">
+        <p className="md:reading-8 whitespace-pre-wrap text-center text-2xl font-semibold leading-9 text-zinc-800 md:whitespace-normal md:text-4xl">
           {`나중에 볼 링크를\n간편하게 저장하고 관리하세요.`}
-        </h2>
-        <h4 className="my-5 whitespace-pre-wrap text-center text-xl font-semibold text-zinc-500 md:whitespace-normal md:text-xl">
+        </p>
+        <p className="my-5 whitespace-pre-wrap text-center text-xl font-semibold text-zinc-500 md:whitespace-normal md:text-xl">
           {`링크가 잊혀지거나\n방치되지 않도록 도와드립니다.`}
-        </h4>
+        </p>
         <Link
           href="/login"
           className="reset-button w-fit rounded-full bg-primary-alt px-6 py-3 text-lg font-semibold text-white md:hover:bg-primary-alt-lighter"
@@ -21,7 +21,7 @@ const TopSection = () => {
         <div className="mt-16 max-w-[950px] overflow-hidden rounded-lg shadow-xl">
           <Image
             src={landing_top}
-            alt="screenshot"
+            alt="linkloud interface image"
             className="h-auto w-full"
             loading="eager"
           />
