d69646980d299cca586ba884d4f0a8ab24efc806
sumin <suminkim.me@gmail.com>
2023-12-17 23:24:15 +0900

    refactor: 화면에 보이는 이미지는 priority 처리하고 나머지는 지연로딩 처리

--- src/features/link/containers/LinkListQueryResult/common/LinkList/DataFetchUI/LoadingUI.tsx
+++ src/features/link/containers/LinkListQueryResult/common/LinkList/DataFetchUI/LoadingUI.tsx
@@ -1,14 +1,12 @@
 'use client';
 
-
 const LinkListLoadingUI = () => {
-
   return (
     <ul
       className={`grid grid-cols-[repeat(auto-fill,minmax(calc(50%-10px),1fr))] grid-rows-[1fr] gap-5 md:grid-cols-[repeat(auto-fill,minmax(270px,1fr))]`}
     >
-      {Array.from({ length: 6 }).map((_, index) => (
-        <li key={index} className="">
+      {Array.from({ length: 10 }).map((_, index) => (
+        <li key={index} className="mb-14">
           <div className="skeleton aspect-[1.91/1] h-auto w-full rounded-lg" />
           <div className="p-2 pt-4">
             <div className="skeleton mb-3 h-3 w-full rounded-xl" />
