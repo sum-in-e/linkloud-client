f2cf2d2f8573c0558995516627b6b0dbca2e24eb
Sumin <suminkim.me@gmail.com>
2024-07-07 21:37:46 +0900

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,16 @@
 import { getSessionWithJwtInServer } from '@/common/modules/utils/session';
 import LogIn from '@/features/auth/login/containers';
-import { redirect } from 'next/navigation';
+import { redirect, notFound } from 'next/navigation';
 
 export const metadata = {
   title: '링클라우드 | 로그인',
 };
 
 export default async function LoginPage() {
+  if (process.env.NEXT_PUBLIC_MODE === 'production') {
+    notFound()
+  }
+  
   const isLogin = await getSessionWithJwtInServer();
 
   if (isLogin) {
