baae3ae94231ba645cc56a5c6822ca0a821d82f6
Sumin <suminkim.me@gmail.com>
2024-07-07 21:37:17 +0900

    Update TopSection.tsx

--- src/features/landing/container/TopSection.tsx
+++ src/features/landing/container/TopSection.tsx
@@ -12,12 +12,12 @@ const TopSection = () => {
         <p className="my-5 whitespace-pre-wrap text-center text-xl font-semibold text-zinc-500 md:whitespace-normal md:text-xl">
           {`링크가 잊혀지거나\n방치되지 않도록 도와드립니다.`}
         </p>
-        <Link
+{/*         <Link
           href="/login"
           className="reset-button w-fit rounded-full bg-primary-alt px-6 py-3 text-lg font-semibold text-white md:hover:bg-primary-alt-lighter"
         >
           무료로 시작하기
-        </Link>
+        </Link> */}
         <div className="mt-16 max-w-[950px] overflow-hidden rounded-lg shadow-xl">
           <Image
             src={landing_top}
