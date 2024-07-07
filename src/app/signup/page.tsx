a5be4d72d639b5bb439ca734164ef58af1c28a6b
Sumin <suminkim.me@gmail.com>
2024-07-07 21:38:35 +0900

    Update page.tsx

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,12 +1,16 @@
 import { getSessionWithJwtInServer } from '@/common/modules/utils/session';
 import SignUp from '@/features/auth/signup/containers';
-import { redirect } from 'next/navigation';
+import { redirect, notFound } from 'next/navigation';
 
 export const metadata = {
   title: '링클라우드 | 회원가입',
 };
 
 export default async function SignUpPage() {
+  if (process.env.NEXT_PUBLIC_MODE === 'production') {
+    notFound()
+  }
+  
   const isLogin = await getSessionWithJwtInServer();
 
   if (isLogin) {
